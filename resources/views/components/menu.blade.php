8e9aec1426e0925706a1d0c431aee071f798c871
Ruben van Erk <ruben@rubenvanerk.nl>
2021-08-13 16:07:05 +0200

    Slightly improve mobile menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -7,7 +7,6 @@
                 </div>
                 <div class="hidden lg:block lg:ml-10">
                     <div class="flex space-x-4">
-                        <!-- Current: "bg-blue-700 text-white", Default: "text-white hover:bg-blue-500 hover:bg-opacity-75" -->
                         <a href="#" class="bg-blue-900 text-white rounded-md py-2 px-3 text-sm font-medium">
                             Competitions
                         </a>
@@ -35,18 +34,16 @@
                     <label for="search" class="sr-only">Search</label>
                     <div class="relative text-gray-400 focus-within:text-gray-600">
                         <div class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-                            <!-- Heroicon name: solid/search -->
-                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                            </svg>
+                            <x-heroicon-s-search class="h-5 w-5"/>
                         </div>
                         <input id="search" class="block w-full bg-white py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-yellow-300 focus:border-white sm:text-sm" placeholder="Search anything..." type="search" name="search">
                     </div>
                 </div>
             </div>
             <div class="flex lg:hidden">
-                <!-- Mobile menu button -->
-                <button x-on:click="open = ! open"  type="button" class="bg-blue-600 p-2 rounded-md inline-flex items-center justify-center text-blue-200 hover:text-white hover:bg-blue-500 hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                <button type="button" aria-controls="mobile-menu" aria-expanded="false" x-bind:aria-expanded="open.toString()"
+                        x-on:click="open = ! open"
+                        class="bg-blue-600 p-2 rounded-md inline-flex items-center justify-center text-blue-200 hover:text-white hover:bg-blue-500 hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-white">
                     <span class="sr-only">Open main menu</span>
                     <x-heroicon-o-menu x-show="!open" class="h-6 w-6"/>
                     <x-heroicon-o-x x-show="open" class="h-6 w-6"/>
@@ -54,7 +51,6 @@
             </div>
             <div class="hidden lg:block lg:ml-4">
                 <div class="flex items-center">
-                    <!-- Profile dropdown -->
                     <div class="ml-3 relative flex-shrink-0">
                         <div>
                             <a class="bg-blue-900 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
@@ -68,10 +64,8 @@
         </div>
     </div>
 
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="lg:hidden" id="mobile-menu" x-show="open">
+    <div class="lg:hidden shadow-2xl" id="mobile-menu" x-show="open">
         <div class="px-2 pt-2 pb-3 space-y-1">
-            <!-- Current: "bg-blue-700 text-white", Default: "text-white hover:bg-blue-500 hover:bg-opacity-75" -->
             <a href="#" class="bg-blue-700 text-white block rounded-md py-2 px-3 text-base font-medium">
                 Competitions
             </a>
