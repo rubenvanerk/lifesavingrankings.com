53eae00e85909d5b4fd31c3126856168b01f223d
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-02-07 19:18:19 +0100

    Fix template

--- resources/views/components/tables/cells/entrants.blade.php
+++ resources/views/components/tables/cells/entrants.blade.php
@@ -1,7 +1,7 @@
 <x-table.cell>
     <div class="flex flex-col space-y-1">
         @foreach($entrants as $entrant)
-            <x-tables.cells.contents.entrants :entrant="$entrant"/>
+            <x-tables.cells.contents.entrant :entrant="$entrant"/>
         @endforeach
     </div>
 </x-table.cell>
