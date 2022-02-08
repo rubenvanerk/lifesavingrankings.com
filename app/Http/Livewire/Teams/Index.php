efc1db393d8ae5c8cd1c2c5ea474d5eaced54dba
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-02-08 11:55:51 +0100

    Update Index.php

--- app/Http/Livewire/Teams/Index.php
+++ app/Http/Livewire/Teams/Index.php
@@ -13,7 +13,7 @@ class Index extends Component
 
     public function render(): View
     {
-        $teams = Team::query()->orderBy('name')->paginate(15);
+        $teams = Team::query()->has('results')->orderBy('name')->paginate(15);
 
         return view('livewire.teams.index', [
             'teams' => $teams,
