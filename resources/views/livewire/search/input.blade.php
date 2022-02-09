4571e4cc5dee402d1f1e018f6dfe0d69fc888f96
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-02-09 08:58:32 +0100

    Autofocus search input on search page

--- resources/views/livewire/search/input.blade.php
+++ resources/views/livewire/search/input.blade.php
@@ -7,7 +7,7 @@
             </div>
             <input id="search"
                    class="block w-full bg-white py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-amber-300 focus:border-white sm:text-sm"
-                   placeholder="{{ __('app.search') }}..." type="search" name="query" @if(request()->routeIs('search')) wire:model="query" @endif>
+                   placeholder="{{ __('app.search') }}..." type="search" name="query" @if(request()->routeIs('search')) wire:model="query" autofocus @endif>
         </div>
     </form>
 </div>
